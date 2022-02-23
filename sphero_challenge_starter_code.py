8d68ca0b396b706abaa1191f196fca04f7441604
ywang24 <94268906+KennethWang2006@users.noreply.github.com>
2022-02-23 10:13:42 +0700

    Update sphero_challenge_starter_code.py

--- sphero_challenge_starter_code.py
+++ sphero_challenge_starter_code.py
@@ -1,3 +1,6 @@
+
+
+# Write your code here :-)
 import board
 import busio
 import time
@@ -10,6 +13,8 @@ sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.B1, echo_pin=board.B0)
 from sphero_rvr import RVRDrive
 
 rvr = RVRDrive(uart = busio.UART(board.A2, board.A3, baudrate=115200))
+
+
 time.sleep(0.5)
 
 rvr.set_all_leds(255,0,0) #set leds to red
@@ -24,7 +29,6 @@ rvr.sensor_start()
 
 print("starting up")
 setpoint = 40.0
-k = 5
 MAX_SPEED = 100
 
 rvr.update_sensors()
@@ -32,36 +36,36 @@ rvr.update_sensors()
 sensor_distance = sonar.distance
 error = 0
 tolerance = 3
-k = 3
+k = 2
 start_time = time.monotonic()
 elapsed_time = time.monotonic() - start_time
 
-def moveControlled(v_time, v_setpoint):
-    while(elapsed_time < v_time):
+#def moveControlled(v_time, v_setpoint):
+#    while(elapsed_time < v_time):
 
-    elapsed_time = time.monotonic() - start_time
+#    elapsed_time = time.monotonic() - start_time
 
-    try:
-        sensor_distance = sonar.distance
+#    try:
+#        sensor_distance = sonar.distance
 
-        # Add your proportional control code here.
-        error = sensor_distance - v_setpoint
-        output = error*k
-        
-        
-        rvr.setMotors(output, output) #set the power of the motors for both the left and right track
-            # Read the Sphero RVR library file to find the rvr.setMotors(left,right) command.
-            # Use this command in the next line to send the output of your proportional
-            # control to both the left and right motors.
+#        # Add your proportional control code here.
+#        error = sensor_distance - v_setpoint
+#        output = error*k
+#        
+#        
+#        rvr.setMotors(output, output) #set the power of the motors for both the left and right track
+#            # Read the Sphero RVR library file to find the rvr.setMotors(left,right) command.
+#            # Use this command in the next line to send the output of your proportional
+#            # control to both the left and right motors.
 
-    except RuntimeError:
-        print("Retrying!")
-        pass
-    time.sleep(0.2)
+#    except RuntimeError:
+ #       print("Retrying!")
+#        pass
+#    time.sleep(0.2)
 
 #-------------------------------------------------------------------------
 #Algorithm 1: Go to orange boxes
-while(elapsed_time < .0):
+while(elapsed_time < 6.0):
 
     elapsed_time = time.monotonic() - start_time
 
@@ -85,7 +89,7 @@ while(elapsed_time < .0):
     
 #Turn 90 degrees clockwise and move drive through the gap (could change with an algorithm but could be repetitive which the function moveControlled could solve)
 #turn 90 degrees clockwise using motor forward and motor backward
-rvr.drive_to_position_si(0,1,0,SPEED) #turn 0 degrees after moving, to these x,y, coordinates at SPEED
+#rvr.drive_to_position_si(0,1,0,SPEED) #turn 0 degrees after moving, to these x,y, coordinates at SPEED
     
 #Algorithm 2: move to location of upper green box
 
